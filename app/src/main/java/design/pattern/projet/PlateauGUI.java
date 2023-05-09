f7b6fc1442515fe546b5c0924d6c9718ad0ab40d
Thomas-ARKADY <thomasdel@arkady.io>
2023-05-09 16:39:52 +0200

    add x y location on click

--- app/src/main/java/design/pattern/projet/PlateauGUI.java
+++ app/src/main/java/design/pattern/projet/PlateauGUI.java
@@ -7,28 +7,44 @@ import java.awt.event.ActionListener;
 
 public class PlateauGUI extends JPanel {
     private int gridSize;
+    private JButton[][] buttons;
 
     public PlateauGUI(int gridSize) {
         this.gridSize = gridSize;
+        buttons = new JButton[gridSize][gridSize];
         initGUI();
     }
 
     private void initGUI() {
         setLayout(new GridLayout(gridSize, gridSize));
 
-        for (int i = 0; i < gridSize * gridSize; i++) {
-            JButton button = new JButton();
-            button.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    System.out.println("Cellule sélectionnée");
-                }
-            });
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                JButton button = new JButton();
+                button.setActionCommand(i + "," + j); // Ajouter les coordonnées x, y au bouton
+                button.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        String[] coordinates = e.getActionCommand().split(",");
+                        int x = Integer.parseInt(coordinates[0]);
+                        int y = Integer.parseInt(coordinates[1]);
+                        System.out.println("Cellule sélectionnée : (" + x + ", " + y + ")");
+                    }
+                });
 
-            button.setBackground(Color.WHITE);
-            button.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-            add(button);
+                button.setBackground(Color.WHITE);
+                button.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                add(button);
+                buttons[i][j] = button;
+            }
         }
     }
-    
+
+    public void setImageAt(int x, int y, ImageIcon icon) {
+        if (x >= 0 && x < gridSize && y >= 0 && y < gridSize) {
+            buttons[x][y].setIcon(icon);
+        } else {
+            throw new IllegalArgumentException("Les coordonnées x et y doivent être comprises entre 0 et " + (gridSize - 1));
+        }
+    }
 }
