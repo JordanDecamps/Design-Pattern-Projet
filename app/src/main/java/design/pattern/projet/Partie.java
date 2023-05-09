7440b694d575158fdf7ac10deb4b53ab13d81e73
cassandradelplanque <cassandra25122002@icloud.com>
2023-05-09 15:15:50 +0200

    ajout classe partie

---
+++ app/src/main/java/design/pattern/projet/Partie.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class Partie {
+    public  List[][] grille;
+    public Coffre coffre;
+    public Inventaire inventaire;
+    public Partie(List[][] grille, Coffre coffre, Inventaire inventaire) {
+        this.grille = grille;
+        this.coffre = coffre;
+        this.inventaire = inventaire;
+    }
+    
+    
+    
+
+}   
