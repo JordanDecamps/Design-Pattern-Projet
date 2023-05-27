92b8dbe975052569a2ae1a2320970b69f271e2ea
jordandecamps <jodecamps59@gmail.com>
2023-05-27 15:21:32 +0200

    modification de la classe grille

--- app/src/main/java/design/pattern/projet/Grille.java
+++ app/src/main/java/design/pattern/projet/Grille.java
@@ -7,7 +7,7 @@ public class Grille {
     public Grille(int x, int y) {
         this.grille = new Case[x][y];
         for (int index = 0; index < x; index++) {
-            for (int index2 = 0; index < y; index++) {
+            for (int index2 = 0; index2 < y; index2++) {
     
                 this.grille[index][index2]= new Case();
             }
